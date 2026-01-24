cd07cd5bc5f1b3c83d833b319b2c7240eb9eb9e6
Mattias Fagerlund <mattias.fagerlund@carretera.se>
2026-01-25 00:22:03 +0100

    Add .gitignore, remove build dir from tracking

--- build/_deps/openmesh-build/src/OpenMesh/Apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/pyqex/build/_deps/openmesh-src/src/OpenMesh/Apps
-# Build directory: C:/Dev/pyqex/build/_deps/openmesh-build/src/OpenMesh/Apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/_deps/openmesh-build/src/OpenMesh/Core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/pyqex/build/_deps/openmesh-src/src/OpenMesh/Core
-# Build directory: C:/Dev/pyqex/build/_deps/openmesh-build/src/OpenMesh/Core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
